c9e9bec0147456b3339941cef570741155deada7
Alex Gorbunov <agorbunov@bekitzur.com>
2019-02-25 22:24:16 +0300

    Add sass and get rid off some pieces of code

---
+++ src/components/header/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Header.sass'
+
+class Header extends Component {
+    render() {
+        return (
+            <header className="App-header">
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+            </header>
+        );
+    }
+}
+
+export default Header;
