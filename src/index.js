2561030573bc922cd85625a5be23515bce680c8f
Alex Gorbunov <agorbunov@bekitzur.com>
2019-03-04 21:44:41 +0300

    Replace HashRouter with BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-import { HashRouter } from 'react-router-dom'
 import { ConnectedRouter } from 'connected-react-router'
 import './index.css';
 import App from './App';
 import configureStore, { history } from './store/configureStore'
+import {BrowserRouter} from 'react-router-dom';
 
 const store = configureStore();
 
 ReactDOM.render(
-    <HashRouter>
+    <BrowserRouter>
         <Provider store={store}>
             <ConnectedRouter history={history}>
                 <App/>
             </ConnectedRouter>
         </Provider>
-    </HashRouter>,
+    </BrowserRouter>,
     document.getElementById('root'),
 );
