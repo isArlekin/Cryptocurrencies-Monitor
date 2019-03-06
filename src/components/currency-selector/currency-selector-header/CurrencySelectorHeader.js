82fa1a8198a7d284041151becd0d8476ef4551a1
Alex Gorbunov <agorbunov@bekitzur.com>
2019-03-06 22:12:01 +0300

    Replace several class component with function components

--- src/components/currency-selector/currency-selector-header/CurrencySelectorHeader.js
+++ src/components/currency-selector/currency-selector-header/CurrencySelectorHeader.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import down from '../../../images/down.svg';
 
-class CurrencySelectorHeader extends Component {
-    render() {
-        const selectedItem = this.props.selectedItem || null;
-        return (
-            <div onClick={this.props.toggle}
-                 className="currency-selector-header">
-                <div className="currency-selector-header__content">
-                    {!selectedItem && <div className="currency-selector-header__placeholder">Select Option</div>}
-                    {selectedItem && <div className="currency-selector-header__value">{ selectedItem.value }</div>}
-                </div>
-                <img className="currency-selector-header__icon" src={down} alt="Down icon of selector component"/>
+function CurrencySelectorHeader(props) {
+    const selectedItem = props.selectedItem || null;
+    return (
+        <div onClick={props.toggle}
+             className="currency-selector-header">
+            <div className="currency-selector-header__content">
+                {!selectedItem && <div className="currency-selector-header__placeholder">Select Option</div>}
+                {selectedItem && <div className="currency-selector-header__value">{ selectedItem.name }</div>}
             </div>
-        );
-    }
+            <img className="currency-selector-header__icon" src={down} alt="Down icon of selector component"/>
+        </div>
+    );
 }
 
 export default CurrencySelectorHeader;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
-
 import './Header.sass'
 
-class Header extends Component {
-    render() {
-        return (
-            <header className="Header">
-                <h1 className="title">Cryptocurrencies Monitor</h1>
+function Header() {
+    return (
+        <header className="Header">
+            <h1 className="title">Cryptocurrencies Monitor</h1>
 
-                <div className="navigation">
-                    <NavLink exact to="/" activeClassName="-active" className='navigation__link'>Top 10</NavLink>
-                    <NavLink to="/dashboard" activeClassName="-active" className='navigation__link'>Dashboard</NavLink>
-                </div>
-            </header>
-        );
-    }
+            <div className="navigation">
+                <NavLink exact to="/" activeClassName="-active" className='navigation__link'>Top 10</NavLink>
+                <NavLink to="/dashboard" activeClassName="-active" className='navigation__link'>Dashboard</NavLink>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
